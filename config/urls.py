444f4d562f2b45ddb5a994e24d9af103f81ba031
reshma@pixdynamics.com <reshma@pixdynamics.com>
2026-02-21 23:08:07 +0530

    feat: initial Django project setup with DRF and Docker

---
+++ config/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/login/", TokenObtainPairView.as_view(), name="login"),
+    path("api/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/", include("snippets.urls")),
+]
